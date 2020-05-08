3532eacae3bbd53c5c46b4313753333874bd21d1
fikriansyah <fikriansyahmaulana1@gmail.com>
2020-05-08 10:43:51 +0700

    upadte index

--- index.php
+++ index.php
@@ -26,22 +26,24 @@ http_response_code($charge_result['http_code']);
 
 echo $charge_result['body'];
 
+
 function chargeAPI($api_url, $server_key, $request_body){
-$ch = curl_init();
+  $ch = curl_init();
   $curl_options = array(
     CURLOPT_URL => $api_url,
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_POST => 1,
     CURLOPT_HEADER => 0,
 
- CURLOPT_HTTPHEADER => array(
+
+    CURLOPT_HTTPHEADER => array(
       'Content-Type: application/json',
       'Accept: application/json',
       'Authorization: Basic ' . base64_encode($server_key . ':')
     ),
     CURLOPT_POSTFIELDS => $request_body
   );
- curl_setopt_array($ch, $curl_options);
+  curl_setopt_array($ch, $curl_options);
   $result = array(
     'body' => curl_exec($ch),
     'http_code' => curl_getinfo($ch, CURLINFO_HTTP_CODE),
